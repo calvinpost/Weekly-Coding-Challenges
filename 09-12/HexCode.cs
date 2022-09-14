194acb352723c0533602a0b7e29c5498546ef955
calvinpost <107434565+calvinpost@users.noreply.github.com>
2022-09-14 14:33:03 -0400

    Create HexCode.cs

---
+++ 09-12/HexCode.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Text.RegularExpressions;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        string[] codes = {
+            "#CD5C5C",
+            "#EAECEE",
+            "#eaecee",
+            "#CD5C58C",
+            "#CD5C5Z",
+            "#CD5C&C",
+            "CD5C5C"
+            };
+        foreach (string code in codes) {
+            Console.WriteLine(code + " : " + IsHexCode(code));
+        }
+    }
+    
+    public static bool IsHexCode(string s) {
+        string p = "^#[0-9a-fA-F]+$";
+        return Regex.IsMatch(s,p);
+    }
+}
